10cd0e1a024a75d4dcd5b98ca4108f1e090137f6
SK-415 <2967923486@qq.com>
2021-07-26 02:58:44 +0800

    add encrypt_params

--- biliapi/utils.py
+++ biliapi/utils.py
@@ -1,8 +1,13 @@
+import time
+from hashlib import md5
 from typing import Any, Dict
-from httpx import AsyncClient
-from httpx._types import URLTypes, ProxiesTypes, HeaderTypes
+from urllib.parse import urlencode
 
-from .exceptions import ResponseCodeException
+from httpx import AsyncClient
+from httpx._models import Response
+from httpx._types import HeaderTypes, ProxiesTypes, URLTypes
+
+from .exceptions import ResponseCodeError
 
 DEFAULT_HEADERS = {
     "user-agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -10,30 +15,62 @@ DEFAULT_HEADERS = {
         "Safari/537.36 Edg/87.0.664.60"),
     "Referer": "https://www.bilibili.com/"
 }
+APPKEY = "4409e2ce8ffd12b8"
+APPSEC = "59b43e04ad6965f34319062b478f83dd"
+
+
+def _encrypt_params(
+    params: Dict[str, Any],
+    local_id: int = 0
+) -> Dict[str, Any]:
+    params['local_id'] = local_id
+    params['appkey'] = APPKEY
+    params['ts'] = int(time.time())
+    params['sign'] = md5(
+        f"{urlencode(sorted(params.items()))}{APPSEC}".encode('utf-8')
+    ).hexdigest()
+    return params
+
+
+async def _request(
+    method: str,
+    url: URLTypes,
+    *,
+    params: Dict[str, Any] = {},
+    encrypt: bool = False,
+    headers: HeaderTypes = DEFAULT_HEADERS,
+    proxies: ProxiesTypes = {"all://": None},
+    **kwargs
+) -> Response:
+    async with AsyncClient(proxies=proxies) as client:
+        resp = await client.request(
+            method,
+            url,
+            headers = headers,
+            params = _encrypt_params(params) if encrypt else params,
+            **kwargs
+        )
+    resp.encoding = "utf-8"
+    return resp
 
 
 async def request(
     method: str,
     url: URLTypes,
     *,
-    headers: HeaderTypes = DEFAULT_HEADERS,
-    proxies: ProxiesTypes = {"all://": None},
+    raw: bool = False,
     **kwargs
 ) -> Dict[str, Any]:
-    """请求基类"""
-
-    async with AsyncClient(proxies=proxies) as client:
-        resp = await client.request(method, url, headers=headers, **kwargs)
-    resp.encoding = "utf-8"
-    raw: Dict = resp.json()
-    if raw['code'] != 0:
-        print(resp.request.url)
-        raise ResponseCodeException(
-            code=raw['code'],
-            msg=raw['message'],
-            data=raw['data']
+    raw_json: Dict[str, Any] = (await _request(method, url, **kwargs)).json()
+    if raw:
+        return raw_json
+    if raw_json['code'] != 0:
+        raise ResponseCodeError(
+            code = raw_json['code'],
+            msg = raw_json['message'],
+            data = raw_json.get('data', None)
         )
-    return raw['data']
+    return raw_json['data']
 
 
 async def get(url: URLTypes, **kwargs):
