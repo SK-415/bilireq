83e5fa7dd1dacad4c5c62671b1aff9a73a9044f5
djkcyl <cyl@cyllive.cn>
2022-09-28 20:32:24 +0800

    补全 grpc 接口的 typing

--- bilireq/grpc/dynamic/__init__.py
+++ bilireq/grpc/dynamic/__init__.py
@@ -1,6 +1,8 @@
 from ..protos.bilibili.ad.v1 import ad_pb2
 from ..protos.bilibili.app.dynamic.v2.dynamic_pb2 import (
+    DynAllReply,
     DynAllReq,
+    DynMixUpListViewMoreReply,
     DynMixUpListViewMoreReq,
     DynSpaceReq,
     DynSpaceRsp,
@@ -19,14 +21,14 @@ async def grpc_get_user_dynamics(
 
 
 @grpc_request
-async def grpc_get_followed_dynamics(**kwargs):
+async def grpc_get_followed_dynamics(**kwargs) -> DynAllReply:
     stub = DynamicStub(kwargs.pop("_channel"))
     req = DynAllReq()
     return await stub.DynAll(req, **kwargs)
 
 
 @grpc_request
-async def grpc_get_followed_dynamic_users(**kwargs):
+async def grpc_get_followed_dynamic_users(**kwargs) -> DynMixUpListViewMoreReply:
     stub = DynamicStub(kwargs.pop("_channel"))
     req = DynMixUpListViewMoreReq(sort_type=1)
     return await stub.DynMixUpListViewMore(req, **kwargs)
