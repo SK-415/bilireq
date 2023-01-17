40e31aae7534ba91efc4d522b7e9a01207fb13f2
艾斯托维亚 <2477547246@qq.com>
2023-01-18 06:05:03 +0800

    更新av与bv互转，添加对视频id的校验

--- bilireq/utils/av_bv.py
+++ bilireq/utils/av_bv.py
@@ -1,5 +1,34 @@
 # TODO 对这部分重构
+from re import compile
 from typing import Union
+
+
+class VideoID:
+    # 哔哩哔哩视频ID类
+    def __init__(self, id: Union[int, str]):
+        if isinstance(id, int):
+            self.aid = id
+            self.bvid = av2bv(id)
+        else:
+            # 构造正则表达式对象
+            av_pattern = compile(r"av[0-9]*")
+            bv_pattern = compile(r"BV1[A-Za-z0-9]{9}")
+            if av_pattern.match(id):
+                self.aid = int(id[2:])
+                self.bvid = av2bv(self.aid)
+            elif bv_pattern.match(id):
+                # 进一步判断是否为合法的视频id
+                if id[5] != "4":
+                    raise ValueError("Invalid video id.")
+                elif id[7] != "1":
+                    raise ValueError("Invalid video id.")
+                elif id[9] != "7":
+                    raise ValueError("Invalid video id.")
+                self.bvid = id
+                self.aid = bv2av(id)
+            else:
+                raise ValueError("Invalid video id.")
+
 
 table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
 tr = {}
@@ -10,9 +39,7 @@ xor = 177451812
 add = 8728348608
 
 
-def av2bv(aid: Union[int, str]) -> str:
-    # TODO 没考虑 av 开头的字符串
-    aid = int(aid)
+def av2bv(aid: int) -> str:
     aid = (aid ^ xor) + add
     r = list("BV1  4 1 7  ")
     for i in range(6):
